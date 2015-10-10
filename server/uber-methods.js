c82be8946b72a278316c3710b7e1d8500f8fcb70
Thomas Jeanneau <thomasj230@gmail.com>
2015-10-11 01:43:04 +0200

    access_token done

--- server/uber-methods.js
+++ server/uber-methods.js
@@ -1,17 +1,28 @@
-TOKEN = Meteor.settings.private.uber.server_token;
+TOKEN_UBER = Meteor.settings.private.uber.server_token;
 ID = Meteor.settings.private.uber.client_id;
 SECRET = Meteor.settings.private.uber.client_secret;
 
+var Uber = Meteor.npmRequire('node-uber');
+var uber = new Uber({
+    client_id: ID,
+    client_secret: SECRET,
+    server_token: TOKEN_UBER,
+    redirect_uri: 'https://ubot.meteor.com/uber',
+    name: 'Slack-Integration'
+});
+
 Meteor.methods({
-  fetchUber: function() {
-      var Uber = Meteor.npmRequire('node-uber');
-      var uber = new Uber({
-          client_id: ID,
-          client_secret: TOKEN,
-          server_token: SECRET,
-          redirect_uri: 'http://localhost:3000',
-          name: 'Slack-Integration'
-      });
-      return HTTP.get(uber.getAuthorizeUrl(['profile']));
-  }
+    fetchUber: function() {
+        return 'https://login.uber.com/oauth/v2/authorize?response_type=code&client_id=' + uber.defaults.client_id;
+    },
+    authUber: function(AUTHORIZATION_CODE){
+        return HTTP.post('https://login.uber.com/oauth/v2/token', {
+            auth: [uber.defaults.client_id, uber.defaults.client_secret].join(':'),
+            params: {
+                redirect_uri: uber.defaults.redirect_uri,
+                code: AUTHORIZATION_CODE,
+                grant_type: 'authorization_code'
+            }
+        })
+    }
 });
