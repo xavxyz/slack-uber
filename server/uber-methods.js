4682de3905a1140f9e2c3f8958926ff27b1fe577
typikal <clcairo@gmail.com>
2015-10-11 04:32:13 +0200

    uber products

--- server/uber-methods.js
+++ server/uber-methods.js
@@ -26,7 +26,7 @@ authUber = function(AUTHORIZATION_CODE){
         }
     }, function(error, success){
         if(success){
-            SUCCESS_TOKEN = success;git push
+            SUCCESS_TOKEN = success;
             postMessage('you are login !');
         }
         if(error){
@@ -35,3 +35,23 @@ authUber = function(AUTHORIZATION_CODE){
     })
 };
 
+getUberProducts = function(lat, lng, type, access_token){
+  var url = "https://sandbox-api.uber.com/v1/products";
+  //var access_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOlsicHJvZmlsZSIsImhpc3RvcnlfbGl0ZSIsImhpc3RvcnkiXSwic3ViIjoiZjBhNzQ0MTMtM2U3Ni00MWE2LWI2NzQtY2RkMjI3MTcxZWZlIiwiaXNzIjoidWJlci11czEiLCJqdGkiOiI2ZDQwZGU4OC02YjFkLTRkNGUtOGMxYy1kNDcyMDI3OTc0OTMiLCJleHAiOjE0NDcxMTI1MDIsImlhdCI6MTQ0NDUyMDUwMiwidWFjdCI6IkFzb3dkbG9CUko0aExrbWNDVUpxV0xZeURyWlI2USIsIm5iZiI6MTQ0NDUyMDQxMiwiYXVkIjoiNGxsYWxqOU5JSXg5S2NsQk9zYnBwT29JMmh3UmVUczkifQ.O6c_v910FQAUsEeDtJVdFBPyWN0ZlIwE46vlgprmCK0JHe5njvbWl0yz22cylH6irMNocCZQIJwQF9-xsPvAbWzQOGOJ8gWyIE4aalcuRErmxiT6IMw_64t32eDBKcHQT8di1L_7h0iQ8gQjvoLP-OqpmG4CflkBNMD38q-Dres9GQDC79mSZWvt-_VNrs3_UDjVjbDOBpvr7rxJ-Nqew4g37oANhKNPUGv104Up1TSyxRf2xjHIVFDUNLSqcBiK6rR_0QuizpwWWT4SzXJf9AY81XmWCcPGoAPzSB4gk_yLC_yCFEryX8kNeYbAo4ozmNLVvFrLdSA7OPW6OVE-EA";
+  var response =  HTTP.get('https://sandbox-api.uber.com/v1/products', {
+        params: {
+            access_token: access_token,
+            latitude: lat,
+            longitude: lng
+        }
+    });
+
+  var list_uber = new Array();
+  for(var i = 0; i < response.data.products.length; i++){
+    if(response.data.products[i].display_name = type){
+        list_uber.push(response.data.products[i])
+    }
+  }
+
+  return list_uber;
+}
