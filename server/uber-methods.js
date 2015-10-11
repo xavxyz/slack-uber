9fb649fb18169c3c8fa5ecafd2293ff3344a071c
xavizalote <xavier.cazalot@gmail.com>
2015-10-11 07:24:32 +0200

    7am commit

--- server/uber-methods.js
+++ server/uber-methods.js
@@ -16,7 +16,7 @@ Meteor.methods({
     fetchUber: function() {
         return 'https://login.uber.com/oauth/v2/authorize?response_type=code&client_id=' + uber.defaults.client_id;
     },
-    authUber: function(AUTHORIZATION_CODE){
+    authUber: function(AUTHORIZATION_CODE) {
         var request =  HTTP.post('https://login.uber.com/oauth/v2/token', {
             auth: [uber.defaults.client_id, uber.defaults.client_secret].join(':'),
             params: {
@@ -26,31 +26,21 @@ Meteor.methods({
             }
         });
         console.log(request);
-        if(request.data.access_token){
-            uber.defaults.success_token = request.data.access_token;
-            postMessage('Logged in with success!');
-        }else{
+        if (request.data.access_token) {
+            SUCCESS_TOKEN = request.data.access_token;
+            var identity = fetchIdentity(SUCCESS_TOKEN);
+            postMessage(identity.first_name +' '+ identity.last_name +' logged in with success!');
+        } else {
             postMessage('Error during login, please try again.');
         }
         return request;
     }
 });
 
-fetchMe = function (accessToken) {
-  try {
-    return Meteor.http.get("https://api.uber.com/v1/me", {
-        headers: { Authorization: 'Bearer ' + accessToken 
-        }
-    }).data;
-  } catch (err) {
-    throw new Error("Failed to fetch identity from Uber. " + err.message);
-  }
-};
-
 getUberProducts = function(lat, lng, type, access_token){
   var url = "https://sandbox-api.uber.com/v1/products";
   //var access_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOlsicHJvZmlsZSIsImhpc3RvcnlfbGl0ZSIsImhpc3RvcnkiXSwic3ViIjoiZjBhNzQ0MTMtM2U3Ni00MWE2LWI2NzQtY2RkMjI3MTcxZWZlIiwiaXNzIjoidWJlci11czEiLCJqdGkiOiI2ZDQwZGU4OC02YjFkLTRkNGUtOGMxYy1kNDcyMDI3OTc0OTMiLCJleHAiOjE0NDcxMTI1MDIsImlhdCI6MTQ0NDUyMDUwMiwidWFjdCI6IkFzb3dkbG9CUko0aExrbWNDVUpxV0xZeURyWlI2USIsIm5iZiI6MTQ0NDUyMDQxMiwiYXVkIjoiNGxsYWxqOU5JSXg5S2NsQk9zYnBwT29JMmh3UmVUczkifQ.O6c_v910FQAUsEeDtJVdFBPyWN0ZlIwE46vlgprmCK0JHe5njvbWl0yz22cylH6irMNocCZQIJwQF9-xsPvAbWzQOGOJ8gWyIE4aalcuRErmxiT6IMw_64t32eDBKcHQT8di1L_7h0iQ8gQjvoLP-OqpmG4CflkBNMD38q-Dres9GQDC79mSZWvt-_VNrs3_UDjVjbDOBpvr7rxJ-Nqew4g37oANhKNPUGv104Up1TSyxRf2xjHIVFDUNLSqcBiK6rR_0QuizpwWWT4SzXJf9AY81XmWCcPGoAPzSB4gk_yLC_yCFEryX8kNeYbAo4ozmNLVvFrLdSA7OPW6OVE-EA";
-  var response =  HTTP.get('https://sandbox-api.uber.com/v1/products', {
+  var response =  HTTP.get(url, {
         params: {
             access_token: access_token,
             latitude: lat,
@@ -66,4 +56,37 @@ getUberProducts = function(lat, lng, type, access_token){
   }
 
   return list_uber;
+};
+
+getPriceEstimates = function(starting, ending, access_token) {
+  var url = "https://api.uber.com/v1/estimates/price";
+  //var access_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOlsicHJvZmlsZSIsImhpc3RvcnlfbGl0ZSIsImhpc3RvcnkiXSwic3ViIjoiZjBhNzQ0MTMtM2U3Ni00MWE2LWI2NzQtY2RkMjI3MTcxZWZlIiwiaXNzIjoidWJlci11czEiLCJqdGkiOiI2ZDQwZGU4OC02YjFkLTRkNGUtOGMxYy1kNDcyMDI3OTc0OTMiLCJleHAiOjE0NDcxMTI1MDIsImlhdCI6MTQ0NDUyMDUwMiwidWFjdCI6IkFzb3dkbG9CUko0aExrbWNDVUpxV0xZeURyWlI2USIsIm5iZiI6MTQ0NDUyMDQxMiwiYXVkIjoiNGxsYWxqOU5JSXg5S2NsQk9zYnBwT29JMmh3UmVUczkifQ.O6c_v910FQAUsEeDtJVdFBPyWN0ZlIwE46vlgprmCK0JHe5njvbWl0yz22cylH6irMNocCZQIJwQF9-xsPvAbWzQOGOJ8gWyIE4aalcuRErmxiT6IMw_64t32eDBKcHQT8di1L_7h0iQ8gQjvoLP-OqpmG4CflkBNMD38q-Dres9GQDC79mSZWvt-_VNrs3_UDjVjbDOBpvr7rxJ-Nqew4g37oANhKNPUGv104Up1TSyxRf2xjHIVFDUNLSqcBiK6rR_0QuizpwWWT4SzXJf9AY81XmWCcPGoAPzSB4gk_yLC_yCFEryX8kNeYbAo4ozmNLVvFrLdSA7OPW6OVE-EA";
+  var response =  HTTP.get(url, {
+        params: {
+            access_token: access_token,
+            start_latitude: starting.latitude,
+            start_longitude: starting.longitude,
+            end_latitude: ending.latitude,
+            end_longitude: ending.longitude
+        }
+    });
+
+  var list_uber = new Array();
+  for(var i = 0; i < response.data.prices.length; i++){
+    if(response.data.products[i].display_name = 'UberX') {
+      list_uber.push(response.data.prices[i])
+    }
+  }
+
+  return list_uber;
+};
+
+fetchIdentity = function (accessToken) {
+  try {
+    return HTTP.get("https://api.uber.com/v1/me", {
+        headers: { Authorization: 'Bearer ' + accessToken }
+    }).data;
+  } catch (err) {
+    throw new Error("Failed to fetch identity from dropbox. " + err.message);
+  }
 };
