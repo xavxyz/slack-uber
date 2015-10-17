ea82dc1c57568fe0f0ee12171938d96dbde10c3c
Thomas Jeanneau <thomasj230@gmail.com>
2015-10-17 12:23:10 +0200

    retour au magnifique code de Xavier

--- server/geoloc.js
+++ server/geoloc.js
@@ -1,12 +1,10 @@
 adressToCoords = function(adress) {
 	var geo = new GeoCoder();
-
+	console.log('adressGeoCoder', adress);
 	var result = geo.geocode(adress);
-
-	var coords = {
+	console.log('resultat Geocode', result);
+	return {
 		latitude: result[0].latitude,
 		longitude: result[0].longitude
 	};
-
-	return coords;
 };
